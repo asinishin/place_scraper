ec81814e72215bf65b7f369a99b7380a8f080ae1
Alex Sinishin <sinishin@gmail.com>
2013-10-20 01:29:17 -0700

    Fied index

--- db/migrate/20131019184452_add_index_to_locations.rb
+++ db/migrate/20131019184452_add_index_to_locations.rb
@@ -1,5 +1,5 @@
 class AddIndexToLocations < ActiveRecord::Migration
   def change
-    add_index(:locations, :lat, :lng, unique: true)
+    add_index(:locations, [:lat, :lng], unique: true)
   end
 end
